68cd219dfddc39fd665228157fe8e1fb45e9c276
shaileyjoseph <shaileyjoseph2006@gmail.com>
2026-02-28 17:10:34 -0800

    fetched and processed rent data in Irvine

--- backend/fetch_data.py
+++ backend/fetch_data.py
@@ -4,31 +4,61 @@ import secret_key
 import urllib
 import time
 
-base_url = "https://api.rentcast.io/v1/listings/sale"
-query_params = {
-    "city" : "Irvine",
-    "state": "CA",
-    "status": "Active",
-    "limit": 500,
-    "offset" : 0,
-}
 
-headers = {
-    "accept": "application/json",
-    "X-Api-Key": secret_key.api_key
-}
-count = 0
-while(count==0 or len(data) > 0):
-    count += 1
-    query_string = urllib.parse.urlencode(query_params)
-    url = base_url + "?" + query_string
-    response = requests.get(url, headers=headers)
-    data = json.loads(response.text)
-    print(len(data), count)
-    query_params["offset"] += 500
-    time.sleep(3)
-    file_path = 'listings/part' + str(count) + '.json'
-    with open(file_path, 'w') as file:
-        json.dump(data, file)
-print(count)
+def fetch_sale_data():
+    base_url = "https://api.rentcast.io/v1/listings/sale"
+    query_params = {
+        "city" : "Irvine",
+        "state": "CA",
+        "status": "Active",
+        "limit": 500,
+        "offset" : 0,
+    }
+    headers = {
+        "accept": "application/json",
+        "X-Api-Key": secret_key.api_key
+    }   
+    count = 0
+    while(count==0 or len(data) > 0):
+        count += 1
+        query_string = urllib.parse.urlencode(query_params)
+        url = base_url + "?" + query_string
+        response = requests.get(url, headers=headers)
+        data = json.loads(response.text)
+        print(len(data), count)
+        query_params["offset"] += 500
+        file_path = 'listings/part' + str(count) + '.json'
+        with open(file_path, 'w') as file:
+            json.dump(data, file)
 
+def fetch_rent_data():
+    base_url = "https://api.rentcast.io/v1/listings/rental/long-term"
+    query_params = {
+        "city" : "Irvine",
+        "state": "CA",
+        "status": "Active",
+        "limit": 500,
+        "offset" : 0,
+        "includeTotalCount":True
+    }
+    headers = {
+        "accept": "application/json",
+        "X-Api-Key": secret_key.api_key
+    }   
+    count = 0
+    while(count==0 or len(data) == 500):
+        count += 1
+        query_string = urllib.parse.urlencode(query_params)
+        url = base_url + "?" + query_string
+        response = requests.get(url, headers=headers)
+        data = json.loads(response.text)
+        print(len(data), count)
+        query_params["offset"] += 500
+        file_path = 'rent_listings/part' + str(count) + '.json'
+        with open(file_path, 'w') as file:
+            json.dump(data, file)
+        if count >= 10:
+            break
+    print(response.headers)
+
+fetch_rent_data()
--- backend/process_data.py
+++ backend/process_data.py
@@ -7,4 +7,28 @@ def combine_data():
     full_df = full_df.reset_index(drop=True)
     full_df.to_json("listings/full_listings.json")
 
+def combine_more_data():
+    df1 = pd.read_json("rent_listings/part1.json")
+    df2 = pd.read_json("rent_listings/part2.json")
+    df3 = pd.read_json("rent_listings/part3.json")
+    df4 = pd.read_json("rent_listings/part4.json")
+    df5 = pd.read_json("rent_listings/part5.json")
+    full_df = pd.concat([df1, df2])
+    full_df = pd.concat([full_df, df3])
+    full_df = pd.concat([full_df, df4])
+    full_df = pd.concat([full_df, df5])
+    full_df = full_df.reset_index(drop=True)
+    full_df.to_json("rent_listings/full_listings.json")
+    
+
+def filter_columns():
+    df = pd.read_json("rent_listings/full_listings.json")
+    df = df.loc[:,["id", "zipCode", "propertyType", "bedrooms", "bathrooms", "squareFootage", "lotSize", "yearBuilt", "price","listedDate"]]
+    print(df.columns)
+    df.dropna()
+    print(df.index)
+    df.to_json("rent_listings/filtered_rent.json")
+
+filter_columns()
+
 
